9ae6f4b49a2e68ee2b587f6f12c112e30418b843
sigeyh <rsigeyh28@gmail.com>
2026-05-07 15:01:22 +0300

    fix: import useEffect in LoginSignup

--- src/pages/LoginSignup.tsx
+++ src/pages/LoginSignup.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
